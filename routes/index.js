8e888e942da505d9650c979aec959de42d5ce13d
indmind <sattanationmail@gmail.com>
2018-03-31 12:04:34 +0700

    add auto increase number if file exist, check files dir every request

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,79 @@
-var express = require("express");
-var ip = require("ip");
-var fs = require("fs")
-var multer = require("multer");
+const express = require("express")
+const ip = require("ip")
+const fs = require("fs")
+const multer = require("multer")
+const path = require('path')
 
-var upload = multer({
+const filedir = path.join(__dirname, '../files/')
+
+const upload = multer({
   storage: multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, "files/");
+    destination: (req, file, cb) => {
+      cb(null, "files/")
     },
-    filename: function(req, file, cb) {
-      cb(null, file.originalname);
-    }
+    filename: handleFileName
   })
-});
+})
 
-const filedir = __dirname + '/../files/'
+const router = express.Router()
 
-var router = express.Router();
+router.use((req, res, next) => {
+  if (!fs.existsSync(filedir)) fs.mkdirSync(filedir)
+  next()
+})
 
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   const files = fs.readdirSync(filedir).map(file => {
-    const stats = fs.statSync(filedir + file)
+    const stats = fs.statSync(path.join(filedir, file))
 
     return {
-      name: file, 
+      name: file,
       size: calculateFileSize(stats.size),
       created: stats.mtimeMs
     }
   }).sort((a, b) => b.created - a.created)
 
-  res.render("index", { files, ip: ip.address(), port: process.env.PORT || "3000" });
-});
+  res.render("index", {
+    files,
+    ip: ip.address(),
+    port: process.env.PORT || "3000"
+  })
+})
 
-router.post("/upload", upload.single("file"), function(req, res, next) {
-  res.redirect('back');
-});
+router.post("/upload", upload.single("file"), (req, res, next) => {
+  res.status(204).end()
+})
 
-router.get("/download/:file", function(req, res, next) {
+router.get("/download/:file", (req, res, next) => {
   const filename = req.params.file
-  console.log("Download:", filename)
 
-  if(!fs.existsSync(filedir + filename)) {
+  if (!fs.existsSync(path.join(filedir, filename))) {
     res.status(404)
-    res.end('Error: file not found')
+    return res.end('Error: file not found')
   }
 
-  res.download(filedir + filename)
-});
+  res.download(path.join(filedir, filename))
+})
 
-router.get("/delete/:file", function(req, res, next) {
+router.get("/delete/:file", (req, res, next) => {
   const filename = req.params.file
-  console.log("Delete:", filename)
 
-  if(!fs.existsSync(filedir + filename)) {
-    console.warn("file not found")
+  if (!fs.existsSync(path.join(filedir, filename))) {
     res.redirect('/')
 
     return
   }
 
-  fs.unlinkSync(filedir + filename)
+  fs.unlinkSync(path.join(filedir, filename))
 
   res.redirect('/')
-});
+})
 
-router.delete("/clear", function(req, res){
+router.delete("/clear", (req, res) => {
   const files = fs.readdirSync(filedir)
 
-  for(const file of files){
-    const filePath = filedir + file
-
-    console.log("Delete", filePath)
+  for (const file of files) {
+    const filePath = path.join(filedir, file)
 
     fs.unlinkSync(filePath)
   }
@@ -78,14 +81,41 @@ router.delete("/clear", function(req, res){
   req.end()
 })
 
+function handleFileName(req, file, cb) {
+  const fileName = file.originalname
+
+  if (!fs.existsSync(path.join(filedir, fileName))) {
+    return cb(null, fileName)
+  }
+
+  const filearr = fileName.split('.')
+  const ext = filearr.pop()
+
+  let newName = `${filearr.join('.')}.${ext}`
+  let numfix
+
+  while (fs.existsSync(path.join(filedir, newName))) {
+    const name = filearr[0]
+
+    numfix = parseInt(name.substr(-1)) + 1
+
+    filearr[0] = numfix && name.substr(-2, 1) == '_' ?
+      name.substr(0, name.length - 1) + numfix :
+      name + '_1'
+
+    newName = `${filearr.join('.')}.${ext}`
+  }
+
+  cb(null, newName)
+}
+
 function calculateFileSize(size) {
   const i = Math.floor(Math.log(size) / Math.log(1024))
 
   return (
-      (size / Math.pow(1024, i)).toFixed(2) * 1 +
-      ' ' +
-      ['B', 'KB', 'MB', 'GB', 'TB'][i]
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i]
   )
 }
 
-module.exports = router;
+module.exports = router
